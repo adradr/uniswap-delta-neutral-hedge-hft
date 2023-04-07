58893fb10de8a708ee63e38409bc42d8d56c5dfe
adradr <adradr@users.noreply.github.com>
2023-04-07 23:46:17 +0000

    🖤 Format code with black

--- test/test_uniswap_math.py
+++ test/test_uniswap_math.py
@@ -1,12 +1,9 @@
-import pytest
-import os
-import sys
 import logging
-import pytest
+
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
 def test_init():
-    return  None
+    return None
