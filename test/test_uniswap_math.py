c956a0c82a0ee53ed0ba0eb97ac64d7438631599
adradr <adrian.lenard@me.com>
2023-04-07 23:44:09 +0200

    🚧 chore(test_uniswap_math.py): add test_init function This commit adds a new test file test_uniswap_math.py with a test_init function. The function is currently empty and will be used to test the initialization of the Uniswap math module.

---
+++ test/test_uniswap_math.py
@@ -0,0 +1,12 @@
+import pytest
+import os
+import sys
+import logging
+import pytest
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+def test_init():
+    return
