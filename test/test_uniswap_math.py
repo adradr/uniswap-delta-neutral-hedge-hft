acae1cd52efd08877a9d5ad5a6fbc527366c4e5c
adradr <adrian.lenard@me.com>
2023-05-14 13:23:09 +0200

    🐛 fix(TokenManagement.py): change function names to snake_case (#7)
    * 🐛 fix(TokenManagement.py): change function names to snake_case
    The function names have been changed to snake_case to follow the Python naming conventions. This improves the readability and maintainability of the code.
    
    * 🔨 refactor(test_uniswap_math.py): rename TokenManager to token_manager and refactor tests
    The TokenManager import is now renamed to TokenManagement to match the package name. The test functions are now refactored to use a pytest fixture to create a token_manager object, which is then used in the test functions. The test function names are also updated to be more descriptive.
    
    * 🖤 Format code with black
    
    ---------
    
    Co-authored-by: adradr <adradr@users.noreply.github.com>

--- test/test_uniswap_math.py
+++ test/test_uniswap_math.py
@@ -1,6 +1,7 @@
 import logging
 
-from ..package.uniswap_math.TokenManagement import TokenManager
+import pytest
+from uniswap_math import TokenManagement
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -9,15 +10,18 @@ ticks = [200240, 200700, 201031]
 prices = [2014.29, 1923.74, 1861.10]
 
 
-def test_manager():
-    return TokenManager(10**18, 10**6)
+@pytest.fixture
+def token_manager():
+    return TokenManagement.TokenManager(10**18, 10**6)
 
 
-def test_price_to_tick(ticks, prices):
+def test_price_to_tick(token_manager):
     for idx, price in enumerate(prices):
-        assert TokenManager.priceToTick(price) == ticks[idx]
+        tick = token_manager.price_to_tick(price)
+        assert tick == ticks[idx]
 
 
-def test_tick_to_price(ticks, prices):
+def test_tick_to_price(token_manager):
     for idx, tick in enumerate(ticks):
-        assert TokenManager.tickToPrice(tick) == prices[idx]
+        price = token_manager.tick_to_price(tick)
+        assert price == prices[idx]
