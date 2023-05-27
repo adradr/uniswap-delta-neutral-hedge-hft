692268ce33fa89b714d2d16d52ed55071e6dfad0
adradr <adrian.lenard@me.com>
2023-05-27 11:38:09 +0200

    🔨 refactor(telegram_handler.py): add type hints and remove unused imports ✨ feat(telegram_handler.py): add debug mode flag 🐛 fix(telegram_handler.py): fix incorrect environment variable names The code has been refactored to include type hints for function parameters and return types. Unused imports have been removed to improve code readability. A debug mode flag has been added to allow the bot to run without making API calls. The environment variable names have been corrected to match the actual names used in the code.

--- package/telegram_interface/telegram_handler.py
+++ package/telegram_interface/telegram_handler.py
@@ -6,7 +6,7 @@ import os
 import time
 import functools
 from dotenv import load_dotenv
-from pathlib import Path
+from typing import Any, NamedTuple, Union
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
@@ -37,12 +37,12 @@ def retry(attempts=5, delay=1):
 
 
 class TelegramAPIHandler:
-    def __init__(self, api_url, debug_mode):
+    def __init__(self, api_url: str, debug_mode: bool) -> None:
         self.api_url = api_url
         self.debug_mode = debug_mode
 
     @retry()
-    def get_jwt_token(self, username, password):
+    def get_jwt_token(self, username: str, password: str) -> Union[str, None]:
         if username or password == "0":
             self.debug_mode = True
             return
@@ -58,10 +58,17 @@ class TelegramAPIHandler:
             return None
 
     @retry()
-    async def _execute_api_command(self, command, context, method="GET", json=None):
+    async def _execute_api_command(
+        self,
+        command: str,
+        update: Update,
+        context,
+        method: str = "GET",
+        json: dict = {},
+    ) -> None:
         if self.debug_mode:
-            await context.bot.send_message(
-                context.from_user.id, f"Executing {command} - DEBUG MODE"
+            await update.message.reply_text(  #  type: ignore
+                f"Executing {command} - DEBUG MODE"
             )
             return
 
@@ -75,24 +82,22 @@ class TelegramAPIHandler:
         await context.bot.send_message(context.from_user.id, message)
         logger.info(f"{command} executed")
 
-    async def start(self, update, context):
-        await self._execute_api_command("start", context)
+    async def start(self, update: Update, context) -> None:
+        await self._execute_api_command("start", update, context)
 
-    async def stop(self, update, context):
-        await self._execute_api_command("stop", context)
+    async def stop(self, update: Update, context) -> None:
+        await self._execute_api_command("stop", update, context)
 
-    async def stats(self, update, context):
-        await self._execute_api_command("stats", context)
+    async def stats(self, update: Update, context) -> None:
+        await self._execute_api_command("stats", update, context)
 
-    async def update_engine(self, update, context):
-        await self._execute_api_command("update", context)
+    async def update_engine(self, update: Update, context) -> None:
+        await self._execute_api_command("update", update, context)
 
-    async def update_params(self, update, context):
-        parts = update.message.text.split(maxsplit=1)
+    async def update_params(self, update: Update, context) -> None:
+        parts = update.message.text.split(maxsplit=1)  # type: ignore
         if len(parts) < 2:
-            await context.bot.send_message(
-                context.from_user.id, "No parameters provided!"
-            )
+            await update.message.reply_text("No parameters provided!")  # type: ignore
             return
 
         command, json_str = parts
@@ -100,51 +105,60 @@ class TelegramAPIHandler:
         try:
             params_dict = json.loads(json_str)
         except json.JSONDecodeError:
-            await context.bot.send_message(
-                context.from_user.id, "Invalid JSON provided!"
-            )
+            await update.message.reply_text("Invalid JSON provided!")  # type: ignore
             return
 
         if not params_dict:
-            await context.bot.send_message(
-                context.from_user.id, "No parameters provided!"
-            )
+            await update.message.reply_text("No parameters provided!")  # type: ignore
             return
 
         await self._execute_api_command(
-            "update_params", context, method="POST", json=params_dict
+            "update_params", update, context, method="POST", json=params_dict
         )
 
 
 if __name__ == "__main__":
-    dotenv_path = "/path/to/your/.env"
-    load_dotenv(dotenv_path) if Path(dotenv_path).exists() else logger.info(
-        "No .env file found."
-    )
-
+    # Load environment variables and parse arguments
+    load_dotenv()
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--username", default=os.getenv("USERNAME"), help="The username for the API"
+        "--username",
+        default=os.getenv("TELEGRAM_ENGINEAPI_USERNAME"),
+        help="The username for the API",
     )
     parser.add_argument(
-        "--password", default=os.getenv("PASSWORD"), help="The password for the API"
+        "--password",
+        default=os.getenv("TELEGRAM_ENGINEAPI_PASSWORD"),
+        help="The password for the API",
     )
     parser.add_argument(
         "--token", default=os.getenv("TELEGRAM_API_KEY"), help="The Telegram bot token"
     )
-    parser.add_argument("--api_url", default=os.getenv("API_URL"), help="The API URL")
+    parser.add_argument(
+        "--api_url", default=os.getenv("TELEGRAM_ENGINEAPI_URL"), help="The API URL"
+    )
+
+    # Add debug flag
+    parser.add_argument(
+        "--debug-mode",
+        action="store_true",
+        help="Start the bot in debug mode (no API calls)",
+    )
+
     args = parser.parse_args()
 
-    if not all([args.username, args.password, args.token, args.api_url]):
+    if not all([args.username, args.password, args.api_url]):
         logger.error("Not all necessary arguments were provided. Exiting...")
         exit(1)
 
     engine_app = TelegramAPIHandler(args.api_url, False)
-    access_token = engine_app.get_jwt_token(args.username, args.password)
-    if not access_token:
-        logger.error("Failed to start bot due to authentication failure")
+    if args.debug_mode:
         engine_app.debug_mode = True
         logger.info("Bot Starting in Debug Mode")
+    else:
+        access_token = engine_app.get_jwt_token(args.username, args.password)
+
+    print(args.token)
 
     app = ApplicationBuilder().token(args.token).build()
     app.add_handler(CommandHandler("start", engine_app.start))
