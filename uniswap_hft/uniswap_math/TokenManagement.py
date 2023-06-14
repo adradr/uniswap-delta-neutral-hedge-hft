8ca36eedfc28786d98599bcf5800f19a0838d048
adradr <adrian.lenard@me.com>
2023-06-15 01:11:05 +0200

    🔨 refactor(TokenManagement.py): remove unused methods and arguments This commit removes the unused methods liquidity0, liquidity1, calc_amount0, calc_amount1, and calculate_amounts from the TokenManager class. It also removes the unused arguments from the constructor and the get_ranges method. This improves the code's readability and maintainability by removing unnecessary code.
    🚀 feat(TokenManagement.py): add functions to calculate liquidity and amounts
    This commit adds several functions to the TokenManager class to calculate liquidity and amounts of token0 and token1. The get_liquidity0 and get_liquidity1 functions calculate liquidity as a function of amounts and price range. The get_liquidity function calculates liquidity as a function of amounts, price range, and decimal places. The get_amount0 and get_amount1 functions calculate the amounts of token0 and token1 as a function of liquidity and price range. The get_amounts function calculates the amounts of token0 and token1 as a function of liquidity, price range, and decimal places. Finally, the calculate_liquidity_amounts and calculate_amounts functions use the aforementioned functions to calculate the amounts of token0 and token1 given a price range and current price.

--- uniswap_hft/uniswap_math/TokenManagement.py
+++ uniswap_hft/uniswap_math/TokenManagement.py
@@ -7,11 +7,19 @@
 """
 
 import math
-from typing import Tuple
+import numpy
+import typing
 
 
 class TokenManager:
-    def __init__(self, token0_decimal: int, token1_decimal: int):
+    def __init__(
+        self,
+        current_price: float,
+        range_pct: float,
+        target_amount: float,
+        token0_decimal: int,
+        token1_decimal: int,
+    ):
         """Initializes the TokenManager class
 
         Additional information:
@@ -31,6 +39,37 @@ class TokenManager:
         self.token0_decimal = token0_decimal
         self.token1_decimal = token1_decimal
         self.Q96: int = 2**96
+
+        # Calculate ranges
+        (
+            self.lower_range,
+            self.current_price,
+            self.upper_range,
+            self.lower_tick,
+            self.current_tick,
+            self.upper_tick,
+        ) = self.get_ranges(
+            percentage=range_pct,
+            current_price=current_price,
+        )
+
+        # Calculate liquidity
+        amount0, amount1 = self.calculate_liquidity_amounts(
+            range_low_price=self.lower_range,
+            range_high_price=self.upper_range,
+            current_price=current_price,
+            target_amount=target_amount,
+        )
+
+        self.liquidity = self.get_liquidity(
+            asqrt=current_price,
+            asqrtA=self.lower_range,
+            asqrtB=self.upper_range,
+            amount0=amount0,
+            amount1=amount1,
+            decimal0=self.token0_decimal,
+            decimal1=self.token1_decimal,
+        )
 
     def tick_to_sqrt_price_x_96(self, tick: int) -> int:
         """Converts a tick to a sqrt price X96, useable by Uniswap V3
@@ -114,70 +153,6 @@ class TokenManager:
             )
         )
 
-    def liquidity0(self, amount: int, pa: int, pb: int) -> float:
-        """
-        Calculates the liquidity value for token0.
-
-        Args:
-            amount (float): The amount of tokens.
-            pa (float): The price of token A.
-            pb (float): The price of token B.
-
-        Returns:
-            float: The liquidity value for token0.
-        """
-        if pa > pb:
-            pa, pb = pb, pa
-        return (amount * (pa * pb) / self.Q96) / (pb - pa)
-
-    def liquidity1(self, amount: int, pa: int, pb: int) -> float:
-        """
-        Calculates the liquidity value for token1.
-
-        Args:
-            amount (float): The amount of tokens.
-            pa (float): The price of token A.
-            pb (float): The price of token B.
-
-        Returns:
-            float: The liquidity value for token1.
-        """
-        if pa > pb:
-            pa, pb = pb, pa
-        return amount * self.Q96 / (pb - pa)
-
-    def calc_amount0(self, liq: int, pa: int, pb: int) -> int:
-        """
-        Calculates the amount of token0.
-
-        Args:
-            liq (float): The liquidity value.
-            pa (float): The price of token A.
-            pb (float): The price of token B.
-
-        Returns:
-            int: The amount of token0.
-        """
-        if pa > pb:
-            pa, pb = pb, pa
-        return int(liq * self.Q96 * (pb - pa) / pa / pb)
-
-    def calc_amount1(self, liq: int, pa: int, pb: int) -> int:
-        """
-        Calculates the amount of token1.
-
-        Args:
-            liq (float): The liquidity value.
-            pa (float): The price of token A.
-            pb (float): The price of token B.
-
-        Returns:
-            int: The amount of token1.
-        """
-        if pa > pb:
-            pa, pb = pb, pa
-        return int(liq * (pb - pa) / self.Q96)
-
     def price_to_sqrtp(self, price: float) -> int:
         """Converts a price to a sqrt price, useable by Uniswap V3
 
@@ -189,50 +164,9 @@ class TokenManager:
         """
         return int(math.sqrt(price) * self.Q96)
 
-    def calculate_amounts(
-        self,
-        current_price: float,
-        range_low: float,
-        range_high: float,
-        total_token0_amount: float,
-    ) -> Tuple[int, int]:
-        """Calculates the amounts of token0 and token1.
-        Requires to use prices in eth format e.g.: 1800 for $1800
-
-        Args:
-            current_price (int): Current price for the pool
-            range_low (int): Price range low
-            range_high (int): Price range high
-            total_token0_amount (int): Total amount of token0
-
-        Returns:
-            Tuple[int, int]: Amounts of token1 and token0 in wei
-        """
-        # calculate sqrt price
-        sqrtp_low = self.price_to_sqrtp(range_low)
-        sqrtp_cur = self.price_to_sqrtp(current_price)
-        sqrtp_upp = self.price_to_sqrtp(range_high)
-
-        # calculate liquidity
-        eth = 10**self.token1_decimal
-        amount_eth = 1 * eth
-        amount_usdc = total_token0_amount * eth
-
-        liq0 = self.liquidity0(amount_eth, sqrtp_cur, sqrtp_upp)
-        liq1 = self.liquidity1(amount_usdc, sqrtp_cur, sqrtp_low)
-        liq = int(min(liq0, liq1))
-
-        # calculate amount0 and amount1
-        amount0 = self.calc_amount0(liq, sqrtp_upp, sqrtp_cur)
-        amount1 = self.calc_amount1(liq, sqrtp_low, sqrtp_cur)
-
-        amount1 = int(amount1 / 10 ** (self.token1_decimal - self.token0_decimal))
-
-        return amount1, amount0
-
     def get_ranges(
-        self, percentage: int, current_price: float
-    ) -> Tuple[float, float, float, int, int, int]:
+        self, percentage: float, current_price: float
+    ) -> typing.Tuple[float, float, float, int, int, int]:
         """
         Gives the lower and upper ranges of a soon to be created pool, given a percentage,
         and current price.
@@ -242,7 +176,7 @@ class TokenManager:
             current_price (float): The current price of the token
 
         Returns:
-            Tuple[float, float, float, int, int, int]: a tuple, containing the lower range,
+            typing.Tuple[float, float, float, int, int, int]: a tuple, containing the lower range,
             current price, upper range, lower tick, current tick, upper tick
         """
         upper_range = current_price * (1 + (percentage / 100))
@@ -258,4 +192,154 @@ class TokenManager:
             lower_tick,
             current_tick,
             upper_tick,
+        )
+
+    # Use 'get_liquidity' function to calculate liquidity as a function of amounts and price range
+    @staticmethod
+    def get_liquidity0(sqrtA, sqrtB, amount0, decimals):
+        if sqrtA > sqrtB:
+            (sqrtA, sqrtB) = (sqrtB, sqrtA)
+
+        liquidity = amount0 / (
+            (2**96 * (sqrtB - sqrtA) / sqrtB / sqrtA) / 10**decimals
+        )
+        return liquidity
+
+    @staticmethod
+    def get_liquidity1(sqrtA, sqrtB, amount1, decimals):
+        if sqrtA > sqrtB:
+            (sqrtA, sqrtB) = (sqrtB, sqrtA)
+
+        liquidity = amount1 / ((sqrtB - sqrtA) / 2**96 / 10**decimals)
+        return liquidity
+
+    @staticmethod
+    def get_liquidity(asqrt, asqrtA, asqrtB, amount0, amount1, decimal0, decimal1):
+        sqrt = (numpy.sqrt(asqrt * 10 ** (decimal1 - decimal0))) * (2**96)
+        sqrtA = numpy.sqrt(asqrtA * 10 ** (decimal1 - decimal0)) * (2**96)
+        sqrtB = numpy.sqrt(asqrtB * 10 ** (decimal1 - decimal0)) * (2**96)
+
+        if sqrtA > sqrtB:
+            (sqrtA, sqrtB) = (sqrtB, sqrtA)
+
+        if sqrt <= sqrtA:
+            liquidity0 = TokenManager.get_liquidity0(sqrtA, sqrtB, amount0, decimal0)
+            return liquidity0
+        elif sqrt < sqrtB and sqrt > sqrtA:
+            liquidity0 = TokenManager.get_liquidity0(sqrt, sqrtB, amount0, decimal0)
+            liquidity1 = TokenManager.get_liquidity1(sqrtA, sqrt, amount1, decimal1)
+            liquidity = liquidity0 if liquidity0 < liquidity1 else liquidity1
+            return liquidity
+
+        else:
+            liquidity1 = TokenManager.get_liquidity1(sqrtA, sqrtB, amount1, decimal1)
+            return liquidity1
+
+    @staticmethod
+    def get_amount0(sqrtA, sqrtB, liquidity, decimals):
+        if sqrtA > sqrtB:
+            (sqrtA, sqrtB) = (sqrtB, sqrtA)
+
+        amount0 = (
+            liquidity * 2**96 * (sqrtB - sqrtA) / sqrtB / sqrtA
+        ) / 10**decimals
+
+        return amount0
+
+    @staticmethod
+    def get_amount1(sqrtA, sqrtB, liquidity, decimals):
+        if sqrtA > sqrtB:
+            (sqrtA, sqrtB) = (sqrtB, sqrtA)
+
+        amount1 = liquidity * (sqrtB - sqrtA) / 2**96 / 10**decimals
+
+        return amount1
+
+    @staticmethod
+    def get_amounts(asqrt, asqrtA, asqrtB, liquidity, decimal0, decimal1):
+        sqrt = (numpy.sqrt(asqrt * 10 ** (decimal1 - decimal0))) * (2**96)
+        sqrtA = numpy.sqrt(asqrtA * 10 ** (decimal1 - decimal0)) * (2**96)
+        sqrtB = numpy.sqrt(asqrtB * 10 ** (decimal1 - decimal0)) * (2**96)
+
+        if sqrtA > sqrtB:
+            (sqrtA, sqrtB) = (sqrtB, sqrtA)
+
+        if sqrt <= sqrtA:
+            amount0 = TokenManager.get_amount0(sqrtA, sqrtB, liquidity, decimal0)
+            return amount0, 0
+
+        elif sqrt < sqrtB and sqrt > sqrtA:
+            amount0 = TokenManager.get_amount0(sqrt, sqrtB, liquidity, decimal0)
+
+            amount1 = TokenManager.get_amount1(sqrtA, sqrt, liquidity, decimal1)
+
+            return amount0, amount1
+
+        else:
+            amount1 = TokenManager.get_amount1(sqrtA, sqrtB, liquidity, decimal1)
+            return 0, amount1
+
+    def calculate_liquidity_amounts(
+        self,
+        range_low_price: float,
+        range_high_price: float,
+        current_price: float,
+        target_amount: float,
+    ) -> typing.Tuple[float, float]:
+        """Calculates amounts of token0 and token1 of an LP with range [range_low_price, range_high_price] at a given price
+
+        Args:
+            range_low_price (float): Range minimum
+            range_high_price (float): Range maximum
+            current_price (float): Current price
+            target_amount (int): Target amount of investment
+
+        Returns:
+            typing.Tuple[float, float]: Returns the amounts of token0, token1
+        """
+        SMIN = numpy.sqrt(
+            range_low_price * 10 ** (self.token1_decimal - self.token0_decimal)
+        )
+        SMAX = numpy.sqrt(
+            range_high_price * 10 ** (self.token1_decimal - self.token0_decimal)
+        )
+
+        sqrt0 = numpy.sqrt(
+            current_price * 10 ** (self.token1_decimal - self.token0_decimal)
+        )
+
+        if SMIN < sqrt0 < SMAX:
+            deltaL = target_amount / (
+                (sqrt0 - SMIN)
+                + ((1 / sqrt0) - (1 / SMAX))
+                * (current_price * 10 ** (self.token1_decimal - self.token0_decimal))
+            )
+            amount1 = deltaL * (sqrt0 - SMIN)
+            amount0 = (
+                deltaL
+                * ((1 / sqrt0) - (1 / SMAX))
+                * 10 ** (self.token1_decimal - self.token0_decimal)
+            )
+        elif sqrt0 < SMIN:
+            deltaL = target_amount / ((1 / SMIN - 1 / SMAX) * current_price)
+            amount1 = 0
+            amount0 = deltaL * (1 / SMIN - 1 / SMAX)
+        else:
+            deltaL = target_amount / (SMAX - SMIN)
+            amount1 = deltaL * (SMAX - SMIN)
+            amount0 = 0
+
+        return amount0, amount1
+
+    def calculate_amounts(
+        self,
+        current_price: float,
+    ) -> typing.Tuple[float, float]:
+        return TokenManager.get_amounts(
+            asqrt=current_price,
+            asqrtA=self.lower_range,
+            asqrtB=self.upper_range,
+            liquidity=self.liquidity,
+            decimal0=self.token0_decimal,
+            decimal1=self.token1_decimal,
         )
