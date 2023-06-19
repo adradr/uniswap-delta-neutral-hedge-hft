8aa40f4d471743b770e3c0c08870d2f191be6432
adradr <adrian.lenard@me.com>
2023-06-19 10:24:19 +0200

    Feature-scheduler (#20)
    * 🚀 feat(setup.py): add new dependencies requests and sqlalchemy
    🚀 feat(scheduler.py): add new module for scheduling trading engine updates and stats retrieval
    🔥 chore(telegram_interface): remove unused file
    🐛 fix(web_manager.py): add logging for closing position due to price being outside of range
    The setup.py file now includes two new dependencies, requests and sqlalchemy. The scheduler.py file is a new module that schedules the trading engine updates and stats retrieval. The telegram_interface/__main__.py file was removed as it was not being used. The web_manager.py file now includes logging for closing a position due to the price being outside of the range.
    
    * 🐛 fix(docker-compose.yml): add uniswap-hft-scheduler service
    ✨ feat(docker-compose.yml): add uniswap-hft-scheduler service to docker-compose file
    The uniswap-hft-scheduler service was added to the docker-compose file. This service runs the uniswap_hft.scheduler.scheduler module, which is responsible for scheduling the execution of the trading bot. The service is dependent on the uniswap-hft-api service, which provides the necessary data for the scheduler to make trading decisions.
    
    * 🖤 Format code with black
    
    ---------
    
    Co-authored-by: adradr <adradr@users.noreply.github.com>

---
+++ uniswap_hft/scheduler/scheduler.py
@@ -0,0 +1,136 @@
+import argparse
+import logging
+import os
+import time
+from typing import Optional
+
+import apscheduler.executors.pool
+import apscheduler.jobstores.sqlalchemy
+import apscheduler.schedulers.background
+import dotenv
+import requests
+
+# Load environment variables
+dotenv.load_dotenv()
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Argument Parser
+parser = argparse.ArgumentParser(description="Trading Engine Update Scheduler")
+parser.add_argument(
+    "--username",
+    type=str,
+    default=os.getenv("SCHEDULER_ENGINEAPI_USERNAME"),
+    help="Username for API authentication",
+)
+parser.add_argument(
+    "--password",
+    type=str,
+    default=os.getenv("SCHEDULER_ENGINEAPI_PASSWORD"),
+    help="Password for API authentication",
+)
+parser.add_argument(
+    "--interval",
+    type=int,
+    default=os.getenv("SCHEDULER_INTERVAL"),
+    help="Scheduler interval in seconds",
+)
+parser.add_argument(
+    "--api-host",
+    type=str,
+    default=os.getenv("SCHEDULER_ENGINEAPI_HOST"),
+    help="Login endpoint",
+)
+parser.add_argument(
+    "--api-port",
+    type=str,
+    default=os.getenv("SCHEDULER_ENGINEAPI_PORT"),
+    help="Login endpoint",
+)
+
+args = parser.parse_args()
+
+HTTP_PREFIX = "http://"
+LOGIN_ENDPOINT = f"{HTTP_PREFIX}{args.api_host}:{args.api_port}/login"
+UPDATE_ENDPOINT = f"{HTTP_PREFIX}{args.api_host}:{args.api_port}/update-engine"
+STATS_ENDPOINT = f"{HTTP_PREFIX}{args.api_host}:{args.api_port}/stats"
+
+
+def get_auth_token(username: str, password: str) -> Optional[str]:
+    """Log in and return the authentication token, or None if login failed."""
+    response = requests.post(
+        LOGIN_ENDPOINT, json={"username": username, "password": password}
+    )
+    if response.status_code == 200:
+        return response.json().get("access_token")
+    else:
+        logging.error("Failed to authenticate: %s", response.text)
+        return None
+
+
+def update_engine(token: str) -> None:
+    """Update the trading engine."""
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.get(UPDATE_ENDPOINT, headers=headers)
+    if response.status_code == 200:
+        logging.info("Successfully updated the trading engine")
+    else:
+        logging.error("Failed to update the trading engine: %s", response.text)
+
+
+def get_stats(token: str) -> None:
+    """Update the trading engine."""
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.get(STATS_ENDPOINT, headers=headers)
+    if response.status_code == 200:
+        logging.info("Successfully retrieved the trading engine stats")
+        logging.info(response.json())
+    else:
+        logging.error("Failed to update the trading engine: %s", response.text)
+
+
+def scheduler_job_update_engine():
+    """Scheduler job function."""
+    token = get_auth_token(args.username, args.password)
+    if token:
+        update_engine(token)
+
+
+def scheduler_job_get_stats():
+    """Scheduler job function."""
+    token = get_auth_token(args.username, args.password)
+    if token:
+        get_stats(token)
+
+
+def main():
+    # Configure the scheduler
+    jobstores = {
+        "default": apscheduler.jobstores.sqlalchemy.SQLAlchemyJobStore(
+            url="sqlite:///jobs.sqlite"
+        ),
+    }
+    executors = {"default": apscheduler.executors.pool.ThreadPoolExecutor(20)}
+    job_defaults = {"coalesce": False, "max_instances": 3}
+    scheduler = apscheduler.schedulers.background.BackgroundScheduler(
+        jobstores=jobstores,
+        executors=executors,
+        job_defaults=job_defaults,
+    )
+    scheduler.add_job(scheduler_job_update_engine, "interval", seconds=args.interval)
+    scheduler.add_job(scheduler_job_get_stats, "interval", seconds=args.interval)
+    scheduler.start()
+    logging.info("Scheduler started with an interval of %d seconds", args.interval)
+
+    try:
+        # This is here to simulate application activity (which keeps the main thread alive).
+        while True:
+            time.sleep(1)
+    except (KeyboardInterrupt, SystemExit):
+        # Not strictly necessary if daemonic mode is enabled but should be done if possible
+        scheduler.shutdown()
+
+
+if __name__ == "__main__":
+    main()
